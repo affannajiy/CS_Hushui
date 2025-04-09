1a856e3903e4dfd5dfaa42cf4c9132f17fc203ed
Affan Najiy <affannajiy@gmail.com>
2025-04-09 23:57:42 +0800

    Update Program.cs

--- MyFirstProgram/MyFirstProgram/Program.cs
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Below was added but the code was more simpler
-namespace MyFirstProgram
+﻿namespace MyFirstProgram
 {
     class Program
     {
